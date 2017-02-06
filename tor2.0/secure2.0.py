3b3eaa2f696363f4146de89c274203e2daeb86ae
danielozer <danielozer22@gmail.com>
2017-02-06 13:57:05 +0200

    secure edit

--- tor2.0/secure2.0.py
+++ tor2.0/secure2.0.py
@@ -1,1 +1,114 @@
 __author__ = "daniel ozer"
+
+                        
+from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import cPickle as pickle
+import hashlib
+
+def get_public_key_from_server(sock,pu_key):
+   return RSA.importKey(pickle.loads(pu_key)
+
+def create_key():
+    random_generator = Random.new().read#get new random key
+    key = RSA.generate(1024, random_generator)#larger is more secure!
+
+    return key
+def Publik_Key(key):
+    """
+    the function create a new public key and return it
+
+    input: addr
+    output:  the public key
+    """
+
+
+    print "The public Key IS:\n ", key.publickey().exportKey()
+    print type(key)
+
+
+    return pickle.dumps(key.publickey().exportKey())
+
+def DecryptMesg(enc_data,key):
+
+    """
+
+    this func recv mesg then it decrypt the mesg using RSA (public key)
+    input: string (mesg ),key
+    output:string mesg
+
+    """
+    binPrivKey = key.exportKey()
+   # print "the private: "+ binPrivKey
+    #enc_data=pickle.loads(enc_data)
+    privKeyObj = RSA.importKey(binPrivKey)
+    return privKeyObj.decrypt(enc_data)
+
+def EncryptMesg(data,pu_key):
+
+    """
+
+    this func recv mesg then it encrypt the mesg using RSA (public key)
+    input: string (mesg ),key
+    output: string mesg encrpyt
+
+    """
+
+    data_encrypt = pu_key.encrypt(data, 32)#32 is random parameter used by RSA
+    print "The Encrypted Data: " , data_encrypt
+    print "*************    " ,type(data_encrypt)
+    return data_encrypt
+
+
+
+
+def EncryptDB(stringMesg,key):
+    """
+    this func recv mesg then it encrypt the mesg using AES
+    input: string (mesg ),key
+    output: string mesg encrpyt
+    """
+
+    encryption_suite = AES.new(key, AES.MODE_CBC, 'This is an IV456')
+    cipher_text = encryption_suite.encrypt(stringMesg)
+    return cipher_text
+
+
+def decryptDB(stringMesg,key):
+    """
+
+    this func recv mesg then it decrypt the mesg using AES
+    input: string (mesg ),key
+    output :string mesg
+
+    """
+
+    decryption_suite = AES.new(key, AES.MODE_CBC, 'This is an IV456')
+    plain_text = decryption_suite.decrypt(stringMesg)
+
+    return  plain_text
+
+
+
+def checksum_md5_text(text):
+
+    """
+
+    this func recv text and then hash it in md5 function
+    input: string text
+    output :string (md5)
+
+    """
+    return hashlib.md5("filename.exe").hexdigest()
+
+def checksum_md5_file(file):
+
+    """
+
+    this func recv file and then hash it in md5 function to get the check sum
+    input: string text
+    output :string (md5)
+
+    """
+    return hashlib.md5(open(file,'rb').read()).hexdigest()
