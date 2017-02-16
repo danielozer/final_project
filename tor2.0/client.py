8c66fc2a628d07c13d3f964f7aad38bdd7d7d1f5
danielozer <danielozer22@gmail.com>
2017-02-16 09:42:04 +0200

    change the client

--- tor2.0/client.py
+++ tor2.0/client.py
@@ -4,6 +4,8 @@ import sendMesg_client
 #import show_page
 import socket
 import cPickle as pickle
+import thread
+
 
 IP="127.0.0.1"
 PORT=9999
@@ -13,9 +15,13 @@ SENDER_NAME="DONI"
 
 class All_userdata:
     def __init__(self):
+
       self.client_key=""
       self.client_public_key=""
       self.server_public_key=""
+      self.loggin=False#true \ false
+      self.mesg_for_send=[] #an array of all the mesg that need  to sent (the mesg is already ready to be send as is.)
+      self.recv_mesg=[]#an array of all the mesg that recieved from the main server
 
 
 def user_data_string(user_name,password):
@@ -27,10 +33,6 @@ def user_data_string(user_name,password):
 def main():
 
   user_data=All_userdata()
-  
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.connect((IP, PORT))
-  #recv the public key of the server as a pickle
 
   key=secure.create_key()
   public_key=secure.Publik_Key(key)
@@ -38,25 +40,76 @@ def main():
   user_data.client_key=key
   user_data.client_public_key=public_key
 
-  pu_key=sock.recv(BUFFER)
 
-  user_data.server_public_key=secure.get_public_key_from_other_side(pu_key)
-  sock.send(user_data.client_public_key)
+
 
   #GET FROM THE USER THE PASSWORD AND USERNAME
-  password="password"
-  sendMesg_client.send_to_server_password(password,sock,user_data.server_public_key,SENDER_NAME)
+  while 1:
 
-  #GET AN ANSWER IF THE PASSWORD IS AUTHORIZED
-  sock.recv(BUFFER)
+      password="password"
+      sendMesg_client.send_to_server_password(password,sock,user_data.server_public_key,SENDER_NAME)
+
+      #GET AN ANSWER IF THE PASSWORD IS AUTHORIZED
+      check=sock.recv(BUFFER)
+
+      while check is True:
+
+          get_data_from_frame()
+
+
+def client_server_recv(clientsock):
+
+
+def client_server(port):
+    #the main server send the port that need to be used
+
+
+    host="127.0.0.1"
+    ADDR = (host, port)
+    serversock = socket(AF_INET, SOCK_STREAM)
+    serversock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    serversock.bind(ADDR)
+    serversock.listen(5)
+
+    while 1:
+        print 'waiting for connection... listening on port', PORT
+        clientsock, addr = serversock.accept()
+        print '...connected from:', addr
+        thread.start_new_thread(client_server_recv, (clientsock))
 
 
 
 
 
 
+def handler_client(user_data):
+    #all the client as a client sender
+    port=9999
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((IP, port))
+
+    sock.send(user_data.client_public_key)
+
+    while 1:
 
 
+def handler_server(user_data):
+    #act as a server for reciving
+    port = 8888
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((IP, port))
+
+
+    pu_key=sock.recv(BUFFER)
+    user_data.server_public_key=secure.get_public_key_from_other_side(pu_key)
+
+
+
+
+
+def control_the_client():
+    #all the usage of the user
+    #its create all the mesg built already
 
 
 
