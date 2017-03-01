5da194a020c2d26ffe832f43447fd7be39e090e7
danielozer <danielozer22@gmail.com>
2017-03-01 19:57:40 +0200

    threads changes

--- tor2.0/client.py
+++ tor2.0/client.py
@@ -38,6 +38,8 @@ def user_usage(user_data):
     #show the interface of the password
     #get_password
 
+def main():
+
 
 def main():
 
@@ -65,11 +67,21 @@ def main():
 
           get_data_from_frame()
 
+def client_to_other_clients(client_sock):
+    #c-client-->c-server
+
+    #def check for right mesg
+    client_sock.send()
+
 
 def client_server_recv(clientsock):
-    print
+    #c-server-->c-client
+    client_recv=clientsock.recv()
+    #insert it to database
 
 def client_server_other_clients(port):
+    #c-server-->c-client
+
     #the main server send the port that need to be used
 
 
@@ -80,13 +92,19 @@ def client_server_other_clients(port):
     serversock.bind(ADDR)
     serversock.listen(5)
 
+
+    print 'waiting for connection... listening on port', PORT
+    clientsock, addr = serversock.accept()
+    print '...connected from:', addr
+
     while 1:
-        print 'waiting for connection... listening on port', PORT
-        clientsock, addr = serversock.accept()
-        print '...connected from:', addr
-        thread.start_new_thread(client_server_recv, (clientsock))
+        recv=clientsock.recv(BUFFER)
+        #put the recv in some db
+
 
 def handler_client_with_server(user_data):
+
+    #c-client-->s-server
     #all the client as a client sender
     port=9999
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -101,18 +119,27 @@ def handler_client_with_server(user_data):
 
         pull_next_mesg=user_data.mesg_for_send[0]
         user_data.mesg_for_send.remove(pull_next_mesg)
-
+        sock.send(pull_next_mesg)
         arr_mesg_lock.release()
 
-def handler_server_only_from_server(user_data):
-    #act as a server for reciving
-    port = 8888
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((IP, port))
+def handler_server_only_from_server(user_data,port):
+    #c-server-->s-client
+    #act as a server for reciving#need to get the port from the server
+    host="127.0.0.1"
+    ADDR = (host, port)
+    serversock = socket(AF_INET, SOCK_STREAM)
+    serversock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    serversock.bind(ADDR)
+    serversock.listen(5)
 
+    while 1:
+        print 'waiting for connection... listening on port', PORT
+        clientsock, addr = serversock.accept()
+        print '...connected from:', addr
+        thread.start_new_thread(client_server_recv, (clientsock))
 
-    pu_key=sock.recv(BUFFER)
-    user_data.server_public_key=secure.get_public_key_from_other_side(pu_key)
+   # pu_key=sock.recv(BUFFER)
+    #user_data.server_public_key=secure.get_public_key_from_other_side(pu_key)
 
 def control_the_client():
     #all the usage of the user
